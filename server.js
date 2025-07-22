61bc6eb71dce03b10e0f3a498ccb0b56046d6d3d
calevminsky <calev@yakirabella.com>
2025-07-22 10:40:39 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const path = require('path');
+app.use(express.static(path.join(__dirname, 'public')));
+
+
 
 app.use(cors());
 app.use(express.json());
@@ -253,8 +257,9 @@ app.get('/debug', async (req, res) => {
 });
 
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
+
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
